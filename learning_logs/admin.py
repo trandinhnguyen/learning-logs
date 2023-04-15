98a088481cbf4c3a495744439a7bcbe3a51bff33
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2023-04-16 02:01:48 +0700

    add new entry

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# The dot before models tells Django to look for models.py in the same directory
+from .models import Topic, Entry
+
+admin.site.register(Topic)
+admin.site.register(Entry)
