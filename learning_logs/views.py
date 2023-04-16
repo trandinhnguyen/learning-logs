2b8a839749db91a1a05200a81bdfeff6eee1ab48
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2023-04-16 23:50:26 +0700

    auth

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -10,22 +12,28 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Showing a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+
+    check_topic_owner(topic, request)
+
     # -date_added in reverse order
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -35,7 +43,9 @@ def new_topic(request):
         # POST data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Display a blank or invalid form
@@ -43,9 +53,12 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
+
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         # No data submitted, create a blank form
@@ -62,3 +75,31 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+@login_required
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    check_topic_owner(topic, request)
+
+    if request.method != 'POST':
+        # Initial request, pre-fill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(topic, request):
+    """Make sure the topic belongs to the current user"""
+    if topic.owner != request.user:
+        raise Http404
